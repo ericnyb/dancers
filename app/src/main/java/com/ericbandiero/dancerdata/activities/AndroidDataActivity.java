3d3ba68d1dfe6d36a370a4b4147cfdbfb32ff59e
ericnyb <ericnyb@aol.com>
2017-11-17 21:24:37 -0500

    RXJAva

--- app/src/main/java/com/ericbandiero/dancerdata/activities/AndroidDataActivity.java
+++ app/src/main/java/com/ericbandiero/dancerdata/activities/AndroidDataActivity.java
@@ -63,6 +63,12 @@ import javax.inject.Provider;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
 
 public class AndroidDataActivity extends Lib_Base_ActionBarActivity implements
 		OnItemClickListener, OnCheckedChangeListener,ITest {
@@ -206,6 +212,50 @@ public class AndroidDataActivity extends Lib_Base_ActionBarActivity implements
 			}
 
 		});
+
+
+
+		///Testing RXJAVA
+		Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
+															   @Override
+
+															   public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+																   //Use onNext to emit each item in the stream//
+																   e.onNext(1);
+																   e.onNext(2);
+																   e.onNext(3);
+																   e.onNext(4);
+
+																   //Once the Observable has emitted all items in the sequence, call onComplete//
+																   e.onComplete();
+															   }
+														   }
+		);
+
+		Observer<Integer> observer = new Observer<Integer>() {
+			@Override
+			public void onSubscribe(Disposable d) {
+				Log.e(TAG, "onSubscribe: ");
+			}
+
+			@Override
+			public void onNext(Integer value) {
+				Log.e(TAG, "onNext: " + value);
+			}
+
+			@Override
+			public void onError(Throwable e) {
+				Log.e(TAG, "onError: ");
+			}
+
+			@Override
+			public void onComplete() {
+				Log.e(TAG, "onComplete: All Done!");
+			}
+		};
+
+//Create our subscription//
+		observable.subscribe(observer);
 	}
 
 	// /End of main
