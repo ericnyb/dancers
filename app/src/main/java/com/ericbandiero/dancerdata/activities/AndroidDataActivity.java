c36b845bcf466fc1a660c40e444c193df3c27bf2
ericnyb <ericnyb@aol.com>
2017-12-10 09:52:10 -0500

    RxJava success with stats queries.
    Formatting.

--- app/src/main/java/com/ericbandiero/dancerdata/activities/AndroidDataActivity.java
+++ app/src/main/java/com/ericbandiero/dancerdata/activities/AndroidDataActivity.java
@@ -375,24 +375,7 @@ public class AndroidDataActivity extends Lib_Base_ActionBarActivity implements
 
 
 		if (item.getTitle() != null && item.getTitle().equals(getString(R.string.menu_venue_by_performance))) {
-
 			dancerDao.getMostShotVenue(this,false);
-
-			/*
-			//ControlStatAdapter controlStatAdapter=new ControlStatAdapter();
-			Intent statIntent=new Intent(this,Lib_StatsActivity.class);
-
-			//These are for the activity
-			statIntent.putExtra(Lib_StatsActivity.EXTRA_STATS_BUILDER, controlStatsActivityBuilderVenueCounts.get());
-
-			//Builder is injected
-			statIntent.putExtra(Lib_StatsActivity.EXTRA_DATA_STATS_ADAPTER_CONTROL_INTERFACE, controlStatsAdapterBuilder);
-
-			//statIntent.putExtra(Lib_StatsActivity.EXTRA_DATA_STATS_ADAPTER_CONTROL_INTERFACE,(Serializable)new ControlStatAdapter());
-
-			startActivity(statIntent);
-			if (AppConstant.DEBUG) Log.d(this.getClass().getSimpleName()+">","Venue picked");
-*/
 		}
 
 		//"Venue By Dance Piece Count"
